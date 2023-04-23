2e88b7e82a424d2469d09013b1784ebad23a2f2f
Ashley Tran <ashleytt@usc.edu>
2023-04-22 21:41:28 -0700

    health sounds

--- Assets/Scripts/HealthPackController.cs
+++ Assets/Scripts/HealthPackController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
 
 public class HealthPackController : MonoBehaviour
 {
     public PlayerController player;
     private int healthPackIncrease = 1;
+    private AudioSource healthSound;
     // Start is called before the first frame update
     void Start()
     {
+        healthSound = gameObject.GetComponent<AudioSource>();
         
     }
 
@@ -24,6 +27,7 @@ public class HealthPackController : MonoBehaviour
 		if (other.gameObject.CompareTag("PlayerBullet") || other.gameObject.CompareTag("Player"))
 		{
             player.GetComponent<PlayerController>().IncreaseHealth(healthPackIncrease);
+            healthSound.Play();
             Destroy(gameObject);
 		}
 	}
@@ -34,6 +38,7 @@ public class HealthPackController : MonoBehaviour
 		if (other.gameObject.CompareTag("PlayerBullet") || other.gameObject.CompareTag("Player"))
 		{
             player.GetComponent<PlayerController>().IncreaseHealth(healthPackIncrease);
+            healthSound.Play();
             Destroy(gameObject);
 		}
 	}

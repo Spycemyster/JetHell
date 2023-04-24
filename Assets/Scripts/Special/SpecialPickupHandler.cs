78ef90e0bf2ad0a8d673e31a2c35a63629ab28d0
Ashley Tran <ashleytt@usc.edu>
2023-04-23 21:04:19 -0700

    Sound for Special Pick Up

--- Assets/Scripts/Special/SpecialPickupHandler.cs
+++ Assets/Scripts/Special/SpecialPickupHandler.cs
@@ -1,19 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+
 
 public class SpecialPickupHandler : MonoBehaviour
 {
     public PlayerController player;
     public int specialItem;
+    private AudioSource ammoSound;
+    private bool pickedUp = false;
 
-	void OnTriggerEnter2D(Collider2D other)
+    void Start() {
+
+        ammoSound = gameObject.GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
 	{
-        Debug.Log("Trigger enter");
-		if (other.gameObject.CompareTag("PlayerBullet") || other.gameObject.CompareTag("Player"))
-		{
-            player.PlayerSetSpecial(specialItem);
-            Destroy(gameObject);
-		}
-	}
+        if (!pickedUp)
+        {
+            Debug.Log("Trigger enter");
+            if (other.gameObject.CompareTag("PlayerBullet") || other.gameObject.CompareTag("Player"))
+            {
+                //player.PlayerSetSpecial(specialItem);
+                //Destroy(gameObject);
+                PickUp();
+            }
+        }
+    }
+
+    public void PickUp()
+    {
+        pickedUp = true;
+        player.PlayerSetSpecial(specialItem);
+        ammoSound.Play();
+        Destroy(gameObject, 1f);
+    }
+
 }
